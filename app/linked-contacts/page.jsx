1635dc568b4011c902949537bf14eff8ad42d100
Dominic Taylor <dom.taylor@defra.gov.uk>
2025-12-11 16:43:34 +0000

    add accessibility tests

---
+++ app/linked-contacts/page.jsx
@@ -0,0 +1,11 @@
+export const metadata = {
+  title: 'Linked Contacts'
+}
+
+export default function LinkedContactsPage({ params, searchParams }) {
+  return (
+    <div className="flex justify-center mt-10">
+      <h1 className="text-3xl font-bold">Linked Contacts</h1>
+    </div>
+  )
+}
