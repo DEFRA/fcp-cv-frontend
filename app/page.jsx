cc81c4d30e35a30ef007d525d74771288c367016
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2025-12-18 13:53:14 +0000

    add link page

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,17 @@
+import Link from 'next/link'
+
 export default function ConsolidatedViewPage() {
   return (
-    <div className="flex justify-center mt-10">
-      <h1 className="text-3xl font-bold">Consolidated View</h1>
+    <div className="m-10">
+      <div>
+        <h1 className="text-3xl font-bold mb-5">Consolidated View</h1>
+        <h2 className="text-xl font-semibold">Apps</h2>
+        <ul className="list-disc list-inside ml-5">
+          <li>
+            <Link href="/linked-contacts">Linked Contacts</Link>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
