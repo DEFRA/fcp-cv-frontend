0920dbfd107efba579ae4b6f2c93a566a49808d0
gmarshd <gordon.marsh@defra.gov.uk>
2026-02-28 13:54:18 +0000

    Added additional tests

--- app/_components/link/AppLink.test.jsx
+++ app/_components/link/AppLink.test.jsx
@@ -12,4 +12,22 @@ describe('AppLink component tests', () => {
     await expect.element(linkElement).toBeInTheDocument()
     await expect.element(linkElement).toHaveAttribute('href', '/authenticate')
   })
+
+  it('renders the AppLink with specified text with no link if no location is specified', async () => {
+    const { getByText, getByRole } = await render(
+      <AppLink text="View Authenticate Questions"/>
+    )
+
+    const linkElement = getByText('View Authenticate Questions')
+    await expect.element(linkElement).toBeInTheDocument()
+    expect(getByRole("link")).not.toBeInTheDocument();
+  })
+
+  it('includes the AppLink even if text is not specified', async () => {
+    const { getByText, getByRole } = await render(
+      <AppLink location="/authenticate"/>
+    )
+    expect(getByRole("link")).toBeInTheDocument();
+  })
+
 })
