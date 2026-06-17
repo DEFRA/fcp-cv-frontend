cfe29b9911219344cca339fae3529d6565e2b971
Chris Salt-Mountain <37069825+csaltmountain@users.noreply.github.com>
2026-06-17 10:17:50 +0100

    fix lint errors

--- app/_components/auth/auth-provider.jsx
+++ app/_components/auth/auth-provider.jsx
@@ -8,7 +8,7 @@ import { createContext, useContext, useState } from 'react'
 const context = createContext({ isDisabled: false, authenticationRequest: {} })
 
 // Create a single instance of PublicClientApplication to re-use during as SSR.
-// Without this, each server render will would create a new PublicClientApplication instance 
+// Without this, each server render will would create a new PublicClientApplication instance
 // and a slow leak in memory usage on the server.
 const ssrPublicClientApplication = new PublicClientApplication({})
 
